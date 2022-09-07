9ddd960bc22239bcfe51a28781f9d4debe450e3a
Onofras Denis <onofras.denisd@gmail.com>
2022-09-07 15:55:24 +0300

    Initial Commit

---
+++ Fortune Wheel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fortune Wheel
+//
+//  Created by Denis Onofras on 07.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fortune Wheel/Fortune_WheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fortune_WheelApp.swift
+//  Fortune Wheel
+//
+//  Created by Denis Onofras on 07.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Fortune_WheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
